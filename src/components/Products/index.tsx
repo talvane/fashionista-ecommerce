9f698362fe951d93235869801549d4e165bfe0df
talvane <talvane.magalhaes@gmail.com>
2020-05-17 22:49:46 -0300

    inclued react-redux

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,46 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { v1 as uuid } from 'uuid';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { getCatalog, Catalog } from '../../services/apiCatalog';
+import { fetchCatalog } from '../../store/reducers/products';
+import { RootState } from '../../store/rootReducer';
 
 import Product from '../Product';
 
 import './styles.scss';
 
 const Products: React.FC = () => {
-  const [produtcs, setProducts] = useState<Catalog>([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  const data = useSelector((state: RootState) => state.products);
 
   useEffect(() => {
-    async function fetchAllProducts() {
-      async function fetchProducts() {
-        const productsResult = await getCatalog();
-        setProducts(productsResult);
-      }
-
-      try {
-        await Promise.all([fetchProducts()]);
-      } catch (error) {
-        console.log(error);
-        throw error;
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    fetchAllProducts();
-  }, []);
+    dispatch(fetchCatalog());
+  }, [dispatch]);
 
   return (
     <section className="products">
-      {isLoading ? (
+      {data.loading ? (
         <h3>Loading...</h3>
       ) : (
         <div className="app__container">
-          <div className="header__title">{produtcs.length} items</div>
+          <div className="header__title">{data.products.length} items</div>
 
           <div className="products__grid">
-            {produtcs.map((product) => (
+            {data.products.map((product) => (
               <Product className="products__box" key={uuid()} {...product} />
             ))}
           </div>
