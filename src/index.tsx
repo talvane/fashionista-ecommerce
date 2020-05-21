1600de4a88ccefa488fbffdb2244f30148bb642c
talvane <talvane.magalhaes@gmail.com>
2020-05-21 11:11:39 -0300

    taken restrict

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './routes';
 import { Provider } from 'react-redux';
 
-import store from './store/store';
+import Routes from './routes';
+
+import { store } from './store/store';
 
 import './styles/main.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Routes />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Routes />
+  </Provider>,
+
   document.getElementById('root')
 );
