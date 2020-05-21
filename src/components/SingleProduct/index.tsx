96dd756491a154684320824eb3cbdf804362c6d3
talvane <talvane.magalhaes@gmail.com>
2020-05-21 19:10:51 -0300

    add component SingleProduct

---
+++ src/components/SingleProduct/index.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
+import ProductImage from '../Product/ProductImage';
+import ProductContent from '../Product/ProductContent';
+import { RootState } from '../../store/rootReducer';
+import {
+  loadProductThunk,
+  sizeSelectorThunk,
+  addToCartThunk,
+} from '../../store/reducers/thunks';
+
+import './styles.scss';
+
+const SingleProduct: React.FC = () => {
+  const dispatch = useDispatch();
+  const params = useParams();
+  const { product, selectedSize } = useSelector(
+    (state: RootState) => state.single
+  );
+  const { sizeIsMissing } = useSelector((state: RootState) => state.cart);
+
+  const selectSizeConnected = (e: any) => {
+    //dispatch(sizeSelectorThunk(e, product.));
+  };
+
+  const addToCartConnected = (e: any) => {
+    dispatch(addToCartThunk(e, selectedSize));
+  };
+
+  useEffect(() => {
+    dispatch(loadProductThunk(params));
+  }, [dispatch, params]);
+
+  return (
+    <div className="single-product">
+      <ProductImage
+        {...product}
+        onSale={product.on_sale}
+        discount={product.discount_percentage}
+      />
+
+      {/*<ProductContent
+        {...product}
+        selectedSize={selectedSize}
+        onClickSize={selectSizeConnected}
+        onClickAdd={addToCartConnected}
+        onSale={product.on_sale}
+        sizeWasNotSelected={sizeIsMissing}
+      />*/}
+    </div>
+  );
+};
+
+export default SingleProduct;
