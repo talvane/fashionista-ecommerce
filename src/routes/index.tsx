1177f2585cd6fa1ee093b48771164860f37e6f16
talvane <talvane.magalhaes@gmail.com>
2020-05-21 11:16:21 -0300

    add ConnectRouter in routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-//import { ConnectedRouter } from 'connected-react-router';
+import { Route, Switch } from 'react-router-dom';
+import { ConnectedRouter } from 'connected-react-router';
 
-//import history from './history';
+import history from './history';
 
 import App from '../components/App/App';
 import Catalog from '../pages/Catalog';
 import Product from '../pages/Product';
 
+//TODO: //{/*<Route path="*" component={Product} /> ->Rota 404<- */}
+
 const Routes: React.FC = () => (
-  <App>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Catalog} />
-        <Route exact path="/produto/:id" component={Product} />
-      </Switch>
-    </BrowserRouter>
-  </App>
-);
-
-export default Routes;
-
-/*
-<ConnectedRouter history={history}>
+  <ConnectedRouter history={history}>
     <App>
       <Switch>
         <Route exact path="/" component={Catalog} />
@@ -30,4 +19,6 @@ export default Routes;
       </Switch>
     </App>
   </ConnectedRouter>
-*/
+);
+
+export default Routes;
