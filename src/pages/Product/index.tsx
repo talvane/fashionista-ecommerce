270411d2a50a0d795e1e88cb469e8d9c7e331cb6
talvane <talvane.magalhaes@gmail.com>
2020-05-21 11:20:47 -0300

    add page Product

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import HeaderDefault from '../../components/Header/HeaderDefault';
+import SingleProduct from '../../components/SingleProduct';
+
 const Product: React.FC = () => {
   return (
     <>
-      <h1>Page Detail Product</h1>
+      <HeaderDefault />
+      <SingleProduct />
     </>
   );
 };
