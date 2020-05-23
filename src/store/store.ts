6b1c38a0f1c7f4bd29d6134974864b4fcd418659
talvane <talvane.magalhaes@gmail.com>
2020-05-23 17:56:31 -0300

    implemented localstorage and load the cart to refresh the page

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,15 +4,24 @@ import { routerMiddleware } from 'connected-react-router';
 
 import rootReducer, { RootState } from './rootReducer';
 import history from '../routes/history';
+import { saveState, loadState } from '../utils/localStorage';
 
 const middleware = [routerMiddleware(history), ...getDefaultMiddleware()];
+const preloadedState = loadState();
 
 export const store = configureStore({
   reducer: rootReducer,
   middleware,
+  preloadedState,
 });
 
 export type AppDispatch = typeof store.dispatch;
+
+store.subscribe(() => {
+  saveState({
+    cart: store.getState().cart,
+  });
+});
 
 export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
 
---
+++ src/utils/localStorage.ts
@@ -0,0 +1,20 @@
+export const saveState = (state: any) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
